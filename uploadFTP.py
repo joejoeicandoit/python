45d14a33051a987a21c05cab65169f4e7e555f65
Cho.Youngjune <42788315+yojucho@users.noreply.github.com>
2021-05-26 13:04:48 +0900

    office-pc
    파일 업로드 소스 작성

---
+++ uploadFTP.py
@@ -0,0 +1,49 @@
+import ftplib
+import logging
+
+def ftp_upload(hostname, username, password, port, upload_src_path, upload_dst_path, timeout):
+    logger.info({
+        'action': 'ftp_upload',
+        'status': 'run'
+    })
+
+    # FTP接続 / アップロード
+    with ftplib.FTP() as ftp:
+        try:
+            ftp.connect(host=hostname, port=port, timeout=timeout)
+            #パッシブモード設定
+            ftp.set_pasv("true")
+            #FTPサーバーログイン
+            ftp.login(username, password)
+            with open(upload_src_path, 'rb') as fp:
+                ftp.storbinary(upload_dst_path, fp)
+
+        except ftplib.all_errors as e:
+            logger.error({
+                'action': 'ftp_upload',
+                'message': 'FTP error = %s' % e
+            })
+
+
+# logの設定
+logger = logging.getLogger(__name__)
+formatter = '%(asctime)s:%(name)s:%(levelname)s:%(message)s'
+logging.basicConfig(
+    filename='./ftp_logger.log',
+    level=logging.DEBUG,
+    format=formatter
+)
+logger.setLevel(logging.INFO)
+
+
+hostname = 'FTPサーバーIP'  # 接続先サーバーのホスト名
+username = 'yojucho'  # ユーザ名
+password = 'ebisu202105!#'  # ログインパスワード
+port = 21  # FTPサーバーのポート
+upload_src_path = 'D:/Temp/itemPackageCustomDataClient.dat'  # アップロードするファイルパス
+upload_dst_path = 'STOR /Teams/PCContents/Title/Lodoss/temp/itemPackageCustomDataClient.dat'  # アップロード先のパス(STOR はアップロードする為のFTPコマンドなので必須)
+timeout = 50
+
+logger.info("===START FTP===")
+ftp_upload(hostname, username, password, port, upload_src_path, upload_dst_path, timeout)
+logger.info("===FINISH FTP===")
