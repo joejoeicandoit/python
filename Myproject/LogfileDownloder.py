7be7e4610bac0e075a07c996285d6cb4fcf27352
曺 榮俊 <yojucho@gameon.office>
2021-07-26 14:52:42 +0900

    Version 1.5

--- Myproject/LogfileDownloder.py
+++ Myproject/LogfileDownloder.py
@@ -1,7 +1,7 @@
-#################################################
+#############################################################
 # FTP 서버에서 파일 다운로드 by. yojucho
-# pyinstaller [Python file] --onefile --noconsole
-#################################################
+# pyinstaller LogfileDownloder.py --onefile --noconsole
+#############################################################
 import os, glob
 from tkinter import *
 from tkinter import filedialog
@@ -111,11 +111,15 @@ def _check_Folder(dirPath, dateValue):
     files = glob.glob(dirPath + "/*")
     
     if len(files) <= 0:
-        messagebox.showwarning("경고", "해당 날짜의 (" + dateValue + ") 로그파일이 존재하지않습니다."
-         + "\n" + "하루 전의 날짜로 입력해보세요 ")
+        messagebox.showwarning("警告", "入力した日付 (" + dateValue + ") のログファイルが存在しません。"
+         + "\n" + "1日前に試してみるか、正確な日付の確認をお願いします。")
+        #messagebox.showwarning("경고", "해당 날짜의 (" + dateValue + ") 로그파일이 존재하지않습니다."
+         #+ "\n" + "하루 전의 날짜로 입력해보세요 ")
     else:
-        downloadPath.configure(justify=LEFT, text="다운로드 경로： " + "\n" + win.dirName)
-        result_label.configure(justify=LEFT, text="로그파일 (" + dateValue + ") 을 다운로드 했습니다.")
+        downloadPath.configure(justify=LEFT, text=" ダウンロード場所： " + "\n" + win.dirName)
+        result_label.configure(justify=LEFT, text=" ログファイル (" + dateValue + ") をダウンロードしました。")
+        #downloadPath.configure(justify=LEFT, text="다운로드 경로： " + "\n" + win.dirName)
+        #result_label.configure(justify=LEFT, text="로그파일 (" + dateValue + ") 을 다운로드 했습니다.")
 
 
 # -----< 다운로드 버튼클릭 >-----
@@ -135,47 +139,60 @@ def download_Click():
     else:
         _init_message()
         _init_dateBox()
-        messagebox.showerror("경고", "올바른 날짜를 (" + dateValue + ") 입력해 주세요")
+        messagebox.showerror("警告", "正しい日付 (" + dateValue + ") を入力してください。")
+        #messagebox.showerror("경고", "올바른 날짜를 (" + dateValue + ") 입력해 주세요")
 # -----< 다운로드 버튼클릭 >-----
 
 
 # App 화면구성:START
 win = Tk()
 win.title("파일 다운로더")
-#win.title("【Title】 LogFile Download")
 win.geometry("540x300+100+100")
 win.resizable(False, False)
 
 # 배경이미지 삽입
 sourcePath= os.path.dirname( os.path.abspath( __file__ ) ) # python 파일의 절대경로 확인
-backImage = PhotoImage(file=sourcePath + r"\backimg.png") # 이미지 파일경로 설정
-canvas = Canvas(bg="black", width=240, height=240)
-canvas.place(x=280, y=50)
-canvas.create_image(0, 0, image=backImage, anchor=NW)
+isfile = os.path.isfile(sourcePath + r"\image\backimg.png")
+if isfile == True:
+    #print("Image True")
+    canvas = Canvas(bg="white",width=240, height=240)
+    backImage = PhotoImage(file=sourcePath + r"\image\backimg.png") # 이미지 파일경로 설정
+    canvas.place(x=280, y=50)
+    canvas.create_image(0, 0, image=backImage, anchor=NW)
+else:
+    #print("Image False")
+    pass
 
 
 # 주의 문구
-alertLbl1 = Label(win, text="주의: 다운로드로 지정한 폴더내의 파일은 삭제 됩니다.")
-alertLbl2 = Label(win, text="주의: 로그 파일이 없는경우에는 하루 전으로 설정해 주세요.")
+alertLbl1 = Label(win, foreground = 'red', text=" ダウンロード場所に指定したフォルダーにファイルがあれば【削除】されます")
+#alertLbl1 = Label(win, text="주의: 다운로드로 지정한 폴더내의 파일은 삭제 됩니다.")
 alertLbl1.place(x=10, y=10)
-alertLbl2.place(x=10, y=30)
 
 # 서버 선택용 라디오 버튼
 radioValue = IntVar()
 radioValue.set(0)
-radioOne = Radiobutton(win, text='PUB환경', variable=radioValue, value=0)
-radioTwo = Radiobutton(win, text='IBT환경', variable=radioValue, value=1)
-radioOne.place(x=10, y=70)
-radioTwo.place(x=100, y=70)
+radioOne = Radiobutton(win, text='PUB(公開環境)', variable=radioValue, value=0)
+radioTwo = Radiobutton(win, text='IBT(社内環境)', variable=radioValue, value=1)
+#radioOne = Radiobutton(win, text='PUB환경', variable=radioValue, value=0)
+#radioTwo = Radiobutton(win, text='IBT환경', variable=radioValue, value=1)
+radioOne.place(x=10, y=60)
+radioTwo.place(x=110, y=60)
 
 # 날자입력용 텍스트 박스
-dateLbl1 = Label (win, text="날짜：")
+dateLbl1 = Label (win, text="日付：")
+#dateLbl1 = Label (win, text="날짜：")
 dateLbl1.place(x=10, y=100)
 vc = win.register(_limit_char)
 dateBox = Entry(win, width=8, textvariable=int, validate="key", validatecommand=(vc, "%P"))
 dateBox.place(x=50, y=100)
-downBtn = Button(win, text="다운로드 시작", command=download_Click)
+downBtn = Button(win, text="ダウンロード開始", command=download_Click)
+#downBtn = Button(win, text="다운로드 시작", command=download_Click)
 downBtn.place(x=120, y=95)
+
+dateLbl2 = Label(win, text="(日付は半角数字で入力してください (20210721))")
+dateLbl2.place(x=10, y=125)
+
 
 # 다운로드 경로 표시용 메세지
 downloadPath = Label(win, text=" ")
